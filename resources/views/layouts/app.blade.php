0fc3f9efc9a47b6773b735885229be597906e6c3
robsonferduda <robsonferduda@gmail.com>
2020-07-13 13:58:56 -0300

    Adequação do rodapé

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,8 +151,8 @@
                                         <li><a href="{{ url('trilhas#lista') }}">TRILHAS</a></li>
                                         <li><a href="{{ url('camping/buscar') }}">CAMPING</a></li>
                                         <li><a href="{{ url('camping/buscar') }}">GUIA DE DIFICULDADE</a></li>
-                                        <li><a href="{{ url('trilhas/fauna-e-flora') }}">FAUNA E FLORA</a>
-                                        </li><li><a href="{{ url('contato') }}">CONTATO</a></li>
+                                        <li><a href="{{ url('trilhas/fauna-e-flora') }}">FAUNA E FLORA</a></li>
+                                        <li><a href="{{ url('contato') }}">CONTATO</a></li>
                                     </ul>
                                 </nav>
                             </div>                  
@@ -213,170 +213,45 @@
 
         <!--Footer Widget Area Start-->
         <div class="footer-widget-area">
-            <div class="container">
+            <div class="container" style="min-height: 300px;">
                 <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-4">
+                    <div class="col-lg-3 col-md-3 col-sm-12">
                         <div class="single-footer-widget contact-text-info">
-                            <h4>Contact Us</h4>
+                            <h4>Contato</h4>
                             <div class="footer-widget-list">
                                 <ul>
-                                    <li class="icon send">19 Charlotte Street, Toronto Ontario, M5V 2H5</li>
-                                    <li class="icon envelope">admin@power-boosts.com</li>
-                                    <li class="icon phone">+ 001 666 8989 55</li>
+                                    <li><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></li>
+                                    <li><i class="fa fa-envelope" aria-hidden="true"></i> trilhasemsc@gmail.com</li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-2 col-md-2 hidden-sm">
-                        <div class="single-footer-widget">
-                            <h4>About Us</h4>
-                            <div class="footer-widget-list">
-                                <ul class="widget-lists">
-                                    <li><a href="#">Why Us</a></li>
-                                    <li><a href="#">Our trips</a></li>
-                                    <li><a href="#">Responsible Business</a></li>
-                                    <li><a href="#">Our History</a></li>
-                                    <li><a href="#">Our Core Values</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 hidden-md hidden-sm">
-                        <div class="single-footer-widget">
-                            <h4>Design Themes</h4>
-                            <div class="footer-widget-list">
-                                <ul class="widget-lists">
-                                    <li><a href="#">Africa</a></li>
-                                    <li><a href="#">Asia</a></li>
-                                    <li><a href="#">Central America</a></li>
-                                    <li><a href="#">Europe</a></li>
-                                    <li><a href="#">Middle East</a></li>
-                                    <li><a href="#">North America</a></li>
-                                    <li><a href="#">Oceania</a></li>
-                                    <li><a href="#">South America</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-3 col-sm-3">
-                        <div class="single-footer-widget">
-                            <h4>Destinations</h4>
-                            <div class="footer-widget-list">
-                                <ul class="widget-lists">
-                                    <li><a href="#">Adventure</a></li>
-                                    <li><a href="#">Hiking and Camping</a></li>
-                                    <li><a href="#">Walking and Trekking</a></li>
-                                    <li><a href="#">Safari Trip</a></li>
-                                    <li><a href="#">Polar</a></li>
-                                    <li><a href="#">Sailing</a></li>
-                                    <li><a href="#">Climbing</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4 col-sm-5">
-                        <div class="single-footer-widget">
-                            <h4>Latest Tweets</h4>
-                            <div class="footer-widget-list twitter-news">
-                                <ul>
-                                    <li><a href="#">@Power-Boosts</a> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremq-ue laudantium. <a href="#">Power-Boosts.com/building-a-website</a></li>
-                                    <li><a href="#">@Power-Boosts</a> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremq-ue laudantium. <a href="#">Power-Boosts.com/building-a-website</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 col-sm-6">
-                        <div class="footer-link">
-                            <a href="#"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-google-plus"></i></a>
-                            <a href="#"><i class="fa fa-linkedin"></i></a>
-                            <a href="#"><i class="fa fa-rss"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-6">
-                        <div class="payment-image">
-                            <img src="img/payment.png" alt="">
-                        </div>    
-                    </div>
+                    </div>                    
                 </div>
             </div>
         </div>
-        <!--End of Footer Widget Area-->
-        <!--Footer Area Start-->
+        
         <div class="footer-area">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4 col-md-5 col-sm-12">
-                        <span>Copyright © 2016 <a href="#">Power-Boosts</a>. All rights reserved.</span>
-                    </div>
-                    <div class="col-lg-8 col-md-7 col-sm-12">
-                        <nav>
-                            <ul id="footer-menu">
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Destinations</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Travel Styles</a></li>
-                                <li><a href="shop-grid-no-sidebar.html">Specials</a></li>
-                                <li><a href="blog-1.html">Blog</a></li>
-                                <li><a href="contact.html">Contact Us</a></li>
-                            </ul>
-                        </nav>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <span>Desde 2020, o site. Trilhas fazemos desde sempre!</span>
                     </div>
                 </div>
             </div>
-        </div>
-        <!--End of Footer Area-->
-        
-        
-        <!-- jquery
-        ============================================ -->        
-        <script src="{{ asset('js/vendor/jquery-1.12.3.min.js') }}"></script>
-        
-        <!-- bootstrap JS
-        ============================================ -->        
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        
-        <!-- nivo slider js
-        ============================================ -->       
+        </div>        
+           
+        <script src="{{ asset('js/vendor/jquery-1.12.3.min.js') }}"></script>     
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>    
         <script src="{{ asset('js/lib/nivo-slider/js/jquery.nivo.slider.js') }}" type="text/javascript"></script>
-        <script src="{{ asset('js/lib/nivo-slider/home.js') }}" type="text/javascript"></script>
-        
-        <!-- wow JS
-        ============================================ -->        
-        <script src="{{ asset('js/wow.min.js') }}"></script>
-        
-        <!-- meanmenu JS
-        ============================================ -->        
-        <script src="{{ asset('js/jquery.meanmenu.js') }}"></script>
-        
-        <!-- owl.carousel JS
-        ============================================ -->        
-        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-        
-        <!-- price-slider JS
-        ============================================ -->        
-        <script src="{{ asset('js/jquery-price-slider.js') }}"></script>
-        
-        <!-- scrollUp JS
-        ============================================ -->        
-        <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
-        
-        <!-- Waypoints JS
-        ============================================ -->        
-        <script src="{{ asset('js/waypoints.min.js') }}"></script>
-        
-        <!-- Counter Up JS
-        ============================================ -->        
-        <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
-        
-        <!-- plugins JS
-        ============================================ -->        
-        <script src="{{ asset('js/plugins.js') }}"></script>
-        
-        <!-- main JS
-        ============================================ -->        
+        <script src="{{ asset('js/lib/nivo-slider/home.js') }}" type="text/javascript"></script>       
+        <script src="{{ asset('js/wow.min.js') }}"></script>       
+        <script src="{{ asset('js/jquery.meanmenu.js') }}"></script>     
+        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>       
+        <script src="{{ asset('js/jquery-price-slider.js') }}"></script>      
+        <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>        
+        <script src="{{ asset('js/waypoints.min.js') }}"></script>        
+        <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>       
+        <script src="{{ asset('js/plugins.js') }}"></script>        
         <script src="{{ asset('js/main.js') }}"></script>
     </body>
-</html>v
+</html>
