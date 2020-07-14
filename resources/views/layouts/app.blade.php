b9fb133951bbbc8f43b2747670df36ad6363453b
rafael01costa <rafael01costa@gmail.com>
2020-07-14 10:36:35 -0300

    crianto footer include

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -214,33 +214,10 @@
         @yield('content')
 
         <!--Footer Widget Area Start-->
-        <div class="footer-widget-area">
-            <div class="container" style="min-height: 300px;">
-                <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-12">
-                        <div class="single-footer-widget contact-text-info">
-                            <h4>Contato</h4>
-                            <div class="footer-widget-list">
-                                <ul>
-                                    <li><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></li>
-                                    <li><i class="fa fa-envelope" aria-hidden="true"></i> trilhasemsc@gmail.com</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>                    
-                </div>
-            </div>
-        </div>
-        
-        <div class="footer-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <span>Desde 2020, o site. Trilhas fazemos desde sempre!</span>
-                    </div>
-                </div>
-            </div>
-        </div>        
+
+        @include('trilhas.componentes.footer')
+
+        <!--End of Footer Area-->     
            
         <script src="{{ asset('js/vendor/jquery-1.12.3.min.js') }}"></script>     
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>    
