1832f4f6b7c16abd8b31ba5de83539d89d044062
robsonferduda <robsonferduda@gmail.com>
2020-07-19 10:35:02 -0300

    Modificaçãoes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('index');
 
-<<<<<<< HEAD
 Route::post('comentario/novo', 'ComentarioController@store');
-=======
+
 Route::get('trilha/add-tags', 'TrilhaController@addTags');
 Route::get('trilhas', 'TrilhaController@searchTrilhas');
 Route::get('{cidade}/trilhas', 'TrilhaController@searchTrilhasCidade');
@@ -25,7 +24,6 @@ Route::get('trilhas/{nivel}', 'TrilhaController@searchTrilhasNivel');
 Route::get('{cidade}/trilhas/{nivel}', 'TrilhaController@searchTrilhasCidadeNivel');
 Route::get('{cidade}/trilhas/{nivel}/{trilha}', 'TrilhaController@searchTrilha');
 Route::get('trilhas/tag/{tag}', 'TrilhaController@searchTrilhasTag');
->>>>>>> 05ca79388a738e9946f328737345549de18f4dd2
 
 Route::get('usuario/add', 'UserController@store');
 Route::get('usuario/update', 'UserController@update');
