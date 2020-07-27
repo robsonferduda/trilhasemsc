16448ede8861a0c0eb9f8c5e2251e9b20e987f5f
robsonferduda <robsonferduda@gmail.com>
2020-07-27 09:27:01 -0300

    Dificuldade

--- resources/views/dificuldade/index.blade.php
+++ resources/views/dificuldade/index.blade.php
@@ -7,14 +7,17 @@
                     <div class="col-md-12">
                         <div class="section-title text-center">
                             <div class="title-border">
-                                <br/><br/><h1>Guia de Dificuldade<span>em Trilhas</span></h1>
+                                <br/><br/><h1>Guia de Dificuldade<span> em Trilhas</span></h1>
                             </div>    
                         </div>
                     </div>  
                     <div class="col-md-12">
                         <a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">ABNT</a>
                         <a href="{{ url('guia-de-dificuldade-em-trilhas/femerj') }}">FEMERJ</a>
-                    </div>                        
+                    </div>      
+                    <div class="col-md-12 center">
+                        <img style="height: 35%" src="{{ asset('img/trilhas/dificuldade/infografico-niveis-de-dificuldade-em-trilhas-vamos-trilhar.png') }}">
+                    </div>                  
                 </div>
             </div>
         </div>
