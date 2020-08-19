a5d2631bf3566da0966336e08194626c6897e22d
Robson Fernando Duda <robsonferduda@gmail.com>
2020-08-19 14:34:12 -0300

    Erro

--- routes/web.php
+++ routes/web.php
@@ -36,26 +36,29 @@ Route::get('camping', 'HomeController@camping');
 Route::get('guia', 'HomeController@guia');
 Route::get('sobre-nos', 'HomeController@sobre');
 
-Route::get('usuario/add', 'UserController@store');
-Route::get('usuario/update', 'UserController@update');
-Route::get('usuarios', 'UserController@index');
-
-Route::get('tag/add', 'TagController@store');
-Route::get('tags', 'TagController@index');
-
-Route::get('tags', 'TagController@index');
-
-Route::get('login/facebook', 'FacebookController@redirectToProvider');
-Route::get('login/facebook/callback', 'FacebookController@handleProviderCallback');
-
-Route::prefix('admin')->group(function () {
-    Route::get('listar-trilhas', 'TrilhaController@index');
-    Route::get('editar-trilha/{id}', 'TrilhaController@editar');
-    Route::get('nova-trilha', 'TrilhaController@novo');
-    Route::post('update-trilha', 'TrilhaController@update');
-    Route::post('create-trilha', 'TrilhaController@create');
-});
-
 Route::get('sitemap', 'SiteMapController@gerar');
 Route::get('sitemap/visualizar/{tipo}', 'SiteMapController@visualizar');
 Route::get('sitemap/visualizar/{tipo}', 'SiteMapController@visualizar');
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('usuario/add', 'UserController@store');
+    Route::get('usuario/update', 'UserController@update');
+    Route::get('usuarios', 'UserController@index');
+
+    Route::get('tag/add', 'TagController@store');
+    Route::get('tags', 'TagController@index');
+
+    Route::get('tags', 'TagController@index');
+
+    Route::get('login/facebook', 'FacebookController@redirectToProvider');
+    Route::get('login/facebook/callback', 'FacebookController@handleProviderCallback');
+
+    Route::prefix('admin')->group(function () {
+        Route::get('listar-trilhas', 'TrilhaController@index');
+        Route::get('editar-trilha/{id}', 'TrilhaController@editar');
+        Route::get('nova-trilha', 'TrilhaController@novo');
+        Route::post('update-trilha', 'TrilhaController@update');
+        Route::post('create-trilha', 'TrilhaController@create');
+    });
+});
