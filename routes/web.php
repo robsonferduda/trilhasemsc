2ca63d92760f3533d380c3c9d1139b734f702c32
rafael01costa <rafael01costa@gmail.com>
2020-08-19 11:03:26 -0300

    Teste

--- routes/web.php
+++ routes/web.php
@@ -13,43 +13,40 @@
 
 Auth::routes();
 
-Route::group(['middleware' => ['web']], function () {
+Route::get('/', 'HomeController@index')->name('index');
+Route::get('home', 'HomeController@index')->name('index');
 
-	Route::get('/', 'HomeController@index')->name('index');
-	Route::get('home', 'HomeController@index')->name('index');
+Route::post('comentario/novo', 'ComentarioController@store');
 
-	Route::post('comentario/novo', 'ComentarioController@store');
+Route::get('trilha/add-tags', 'TrilhaController@addTags');
+Route::get('trilhas', 'TrilhaController@searchTrilhas');
+Route::get('{cidade}/trilhas', 'TrilhaController@searchTrilhasCidade');
+Route::get('trilhas/{nivel}', 'TrilhaController@searchTrilhasNivel');
+Route::get('{cidade}/trilhas/{nivel}', 'TrilhaController@searchTrilhasCidadeNivel');
+Route::get('{cidade}/trilhas/{nivel}/{trilha}', 'TrilhaController@searchTrilha');
+Route::get('trilhas/tag/{tag}', 'TrilhaController@searchTrilhasTag');
 
-	Route::get('trilha/add-tags', 'TrilhaController@addTags');
-	Route::get('trilhas', 'TrilhaController@searchTrilhas');
-	Route::get('{cidade}/trilhas', 'TrilhaController@searchTrilhasCidade');
-	Route::get('trilhas/{nivel}', 'TrilhaController@searchTrilhasNivel');
-	Route::get('{cidade}/trilhas/{nivel}', 'TrilhaController@searchTrilhasCidadeNivel');
-	Route::get('{cidade}/trilhas/{nivel}/{trilha}', 'TrilhaController@searchTrilha');
-	Route::get('trilhas/tag/{tag}', 'TrilhaController@searchTrilhasTag');
+Route::get('guia-de-dificuldade-em-trilhas', 'DificuldadeController@index');
+Route::get('guia-de-dificuldade-em-trilhas/abnt', 'DificuldadeController@abnt');
+Route::get('guia-de-dificuldade-em-trilhas/femerj', 'DificuldadeController@femerj');
 
-	Route::get('guia-de-dificuldade-em-trilhas', 'DificuldadeController@index');
-	Route::get('guia-de-dificuldade-em-trilhas/abnt', 'DificuldadeController@abnt');
-	Route::get('guia-de-dificuldade-em-trilhas/femerj', 'DificuldadeController@femerj');
+Route::get('galerias', 'GaleriaController@index');
+Route::get('contato', 'HomeController@contato');
+Route::get('camping', 'HomeController@camping');
+Route::get('guia', 'HomeController@guia');
+Route::get('sobre-nos', 'HomeController@sobre');
 
-	Route::get('galerias', 'GaleriaController@index');
-	Route::get('contato', 'HomeController@contato');
-	Route::get('camping', 'HomeController@camping');
-	Route::get('guia', 'HomeController@guia');
-	Route::get('sobre-nos', 'HomeController@sobre');
+Route::get('usuario/add', 'UserController@store');
+Route::get('usuario/update', 'UserController@update');
+Route::get('usuarios', 'UserController@index');
 
-	Route::get('usuario/add', 'UserController@store');
-	Route::get('usuario/update', 'UserController@update');
-	Route::get('usuarios', 'UserController@index');
+Route::get('tag/add', 'TagController@store');
+Route::get('tags', 'TagController@index');
 
-	Route::get('tag/add', 'TagController@store');
-	Route::get('tags', 'TagController@index');
+Route::get('tags', 'TagController@index');
 
-	Route::get('tags', 'TagController@index');
-
-	Route::get('login/facebook', 'FacebookController@redirectToProvider');
-	Route::get('login/facebook/callback', 'FacebookController@handleProviderCallback');
-});
+Route::get('login/facebook', 'FacebookController@redirectToProvider');
+Route::get('login/facebook/callback', 'FacebookController@handleProviderCallback');
 
 Route::prefix('admin')->group(function () {
     Route::get('listar-trilhas', 'TrilhaController@index');
