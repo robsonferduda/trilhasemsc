65bf4aadcb682f9abf9c1c2e291c4ced12dd7128
Robson Fernando Duda <robsonferduda@gmail.com>
2020-12-08 09:33:45 -0300

    Fotos Trilha

--- resources/views/admin/trilha/editar.blade.php
+++ resources/views/admin/trilha/editar.blade.php
@@ -230,7 +230,19 @@
                         </div>
                     </div>
 
-                   
+                    <div class="row clearfix">
+                        <div class="col-lg-12 col-md-12 col-sm-12" style="margin-top: 20px;">
+                            <h5>Fotos para Postagem</h5>
+                                <textarea>
+                                    @forelse($trilha->foto as $foto)
+                                        <p><img alt="" src="https://trilhasemsc.com.br/public/img/trilhas/descricao.jpg" /></p>
+                                    @empty
+                                        Nenhuma foto cadastrada
+                                    @endforelse
+                                </textarea>
+                            </div>           
+                        </div>
+                    </div>
                 </div>
 	    </div>
 	</div>       
