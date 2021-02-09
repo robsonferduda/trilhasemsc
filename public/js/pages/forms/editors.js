2de50d2891caf3c3192cdf1f140975953e5e0d66
Robson Fernando Duda <robsonferduda@gmail.com>
2021-02-09 14:40:55 -0300

    Ckeditor

--- public/js/pages/forms/editors.js
+++ public/js/pages/forms/editors.js
@@ -6,14 +6,9 @@ $(function () {
             { name: 'document',	   groups: [ 'mode', 'document' ] },			// Displays document group with its two subgroups.
              { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },			// Group's name will be used to create voice label.
              '/',																// Line break - next group will be placed in new line.
-             
-             { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
-                { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-                { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-                { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
-                '/',
-                { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-                { name: 'colors', items: [ 'TextColor', 'BGColor' ] }
+             { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+             { name: 'links' },
+             { name: 'colors', items: [ 'TextColor', 'BGColor' ] }
         ]
     
         // NOTE: Remember to leave 'toolbar' property with the default value (null).
