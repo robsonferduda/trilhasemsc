2b3f49147ed81213ebe164abf5cbb9f2ffb744d7
rafael01costa <rafael01costa@gmail.com>
2021-02-18 14:36:06 -0300

    Retirando os menu Galerias

--- resources/views/trilhas/componentes/menu_mobile.blade.php
+++ resources/views/trilhas/componentes/menu_mobile.blade.php
@@ -3,7 +3,6 @@
         <li><a href="{{ url('/') }}">HOME</a></li>
         <li><a href="{{ url('trilhas/#lista') }}">TRILHAS</a></li>
         <li><a href="{{ url('camping') }}">CAMPING</a></li>
-        <li><a href="{{ url('galerias') }}">GALERIAS</a></li>
         <li><a href="{{ url('guia-de-dificuldade-em-trilhas') }}">GUIA DE DIFICULDADE</a></li>
     </ul>
 </nav>
