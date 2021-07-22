d6101da05c754f4c08819bd4fef524e1e38c40ef
rafael01costa <rafael01costa@gmail.com>
2021-07-22 15:55:47 -0300

    Ads guia de dificuldade

--- resources/views/dificuldade/index.blade.php
+++ resources/views/dificuldade/index.blade.php
@@ -7,6 +7,16 @@
         <div class="blog-post-area section-padding">
             <div class="container">
                 <div class="row">
+                    <!-- Horizontal Tela Detalhes Trilha -->
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-1229685353625953"
+                        data-ad-slot="7739149091"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                     <div class="col-md-12">
                         <div class="section-title text-center">
                             <div class="title-border">
