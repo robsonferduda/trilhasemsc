4f22c9815eba388e39ec1089f2bc180a060041ef
rafael01costa <rafael01costa@gmail.com>
2021-07-22 14:44:53 -0300

    Ads Bloco

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,21 +29,21 @@
                         <p>
                             Agradecemos se leu até aqui! As próximas leituras serão mais empolgantes, pois trarão nelas os detalhes e imagens de Santa Catarina em trilhas!
                         </p>
-                    </div>
-                    <!-- Horizontal Tela Detalhes Trilha -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-1229685353625953"
-                        data-ad-slot="7739149091"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
+                    </div>                
                 </div>
             </div>
         </div>
-      
+        <!-- Horizontal Tela Detalhes Trilha -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-1229685353625953"
+            data-ad-slot="7739149091"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+
         @include('trilhas.componentes.ultima_trilha')
         
          <!--Best Sell Area Start-->
