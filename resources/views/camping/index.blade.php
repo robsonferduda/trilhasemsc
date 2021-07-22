e268c6821289a8abdc333378603cbb76d8d7baa3
rafael01costa <rafael01costa@gmail.com>
2021-07-22 15:24:56 -0300

    Ads Bloco

--- resources/views/camping/index.blade.php
+++ resources/views/camping/index.blade.php
@@ -174,19 +174,19 @@
                         </div>                        
                     </div>
             </div>
-        </div>
-        <div style='margin-bottom: 50px;'>
-            <!-- Horizontal Tela Detalhes Trilha -->
-            <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-1229685353625953"
-                data-ad-slot="7739149091"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-        </div>
+            <div style='margin-bottom: 50px;'>
+                <!-- Horizontal Tela Detalhes Trilha -->
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-1229685353625953"
+                    data-ad-slot="7739149091"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+        </div>       
     </div>
 </div>
 @endsection
