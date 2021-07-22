38f25f5404af871793ac47b201ee57ee67f277d0
rafael01costa <rafael01costa@gmail.com>
2021-07-22 14:03:51 -0300

    Ads Bloco

--- resources/views/trilhas/detalhes.blade.php
+++ resources/views/trilhas/detalhes.blade.php
@@ -68,6 +68,16 @@
                                 </div>
                             </div>
                         </div>
+                        <!-- Horizontal Tela Detalhes Trilha -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-1229685353625953"
+                            data-ad-slot="7739149091"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                         <div class="blog-comments" id="comentarios">
                            <h4 class="blog-title">COMENTÁRIOS DOS <span>TRILHEIROS</span></h4>
 
