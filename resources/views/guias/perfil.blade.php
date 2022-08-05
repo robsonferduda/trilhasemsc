a6875b84263b7743b721fab17f7f5ef16b02a279
Robson <robson@coperve.ufsc.br>
2022-08-05 10:43:35 -0300

    Listagem de Guias

--- resources/views/guias/perfil.blade.php
+++ resources/views/guias/perfil.blade.php
@@ -11,7 +11,25 @@
                         </div>
                     </div>                          
                 </div>
-                
+                <div class="row">
+                    <div class="col-md-3">
+                        @if($guia->nm_path_logo_gui)
+                            <img class="circle-image mb-3 mb-md-0" src="{{ asset('img/guias/'.$guia->nm_path_logo_gui) }}" alt="Logo Guia {{ $guia->nm_guia_gui }}">
+                        @else
+                            <img class="circle-image mb-3 mb-md-0" src="{{ asset('img/guias/default.png') }}" alt="Logo Guia {{ $guia->nm_guia_gui }}">
+                        @endif
+                    </div>
+                    <div class="col-md-9">
+                        <h4>{{ $guia->nm_guia_gui }}</h4>
+                        <p><i class="fa fa-instagram"></i> <a href="{{ url("guia/perfil/estatistica/instagram", $guia->id_guia_gui) }}">{{ $guia->nm_instagram_gui }}</a></p>
+                        <p><strong>Cidade</strong>: {{ $guia->origem->nm_cidade_cde }}</p>
+                        <p><strong>Contato</strong>: {{ ($guia->fone) ? $guia->fone->nu_fone_fon : '' }}</p>
+                        <p>
+                            {{ $guia->dc_biografia_gui }}
+                        </p>
+                        <a class="btn btn-success" href="{{ url("guia/perfil/estatistica/telefone", $guia->id_guia_gui) }}"><i class="fa fa-whatsapp" aria-hidden="true"></i> Enviar Mensagem</a>
+                    </div>
+                </div>                
             </div>
         </div>
 @endsection
