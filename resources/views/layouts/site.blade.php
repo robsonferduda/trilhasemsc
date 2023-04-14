b681207538265edd39e3e6f16ef271c003e1336d
Robson Fernando Duda <robsonferduda@gmail.com>
2023-04-14 14:49:59 -0300

    Atualização Novo

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -335,15 +335,15 @@
                <!-- -------- START HEADER 5 w/ text and illustration ------- -->
                <div class="container">
                   <div class="row">
-                     <div class="col-lg-3 my-auto">
-                        <h1 class="mb-0 text-primary">Trilhas </h1>
-                        <h1 class="mb-4 text-primary">Favoritas</h1>
+                     <div class="col-lg-2 my-auto text-center">
+                        <h2 class="mb-0 text-primary">Trilhas </h2>
+                        <h2 class="mb-4 text-primary">Favoritas</h2>
                         <p class="lead">Essas são as trilhas favoritas dos trilherios de Santa Catarina</p>
                         <div class="buttons">
                            <button type="button" class="btn bg-gradient-warning mt-4">Saiba Mais</button>
                         </div>
                      </div>
-                     <div class="col-lg-9 ps-5 pe-0">
+                     <div class="col-lg-10 ps-5 pe-0">
                         <div class="row">
                             <div class="col-lg-3 col-6">
                               <img class="w-100 border-radius-lg h-50 shadow mt-0 mt-lg-7" src="{{ asset('img/trilhas/destaque-pequena/'.$preferidas[0]->foto->where('id_tipo_foto_tfo',1)->first()->nm_path_fot) }}" alt="">
