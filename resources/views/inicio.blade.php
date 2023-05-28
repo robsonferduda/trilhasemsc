2f3bf94630c2052994613ad7ed5cffd866b00677
Robson Fernando Duda <robsonferduda@gmail.com>
2023-05-28 17:39:47 -0300

    Novo

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.site')
 @section('content')
     @include('inicio/destaques')
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-1229685353625953"
-        data-ad-slot="7739149091"
-        data-ad-format="auto"
-        data-full-width-responsive="true">
-    </ins>
-    <script> (adsbygoogle = window.adsbygoogle || []).push({});  </script>  
+    <section class="pt-1 pb-0 mt-3">
+        <div class="container">
+           <div class="row mb-2">
+              <div class="col-md-7">
+                 <h3 class="text-info">Condutores de Aventura</h3>
+              </div>
+           </div>
+           <div class="row">
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-1229685353625953"
+                    data-ad-slot="7739149091"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true">
+                </ins>
+                <script> (adsbygoogle = window.adsbygoogle || []).push({});  </script>  
+            </div>
+        </div>
+    </section>
     @include('inicio/eventos')
     @include('inicio/condutores')
     @include('inicio/favoritos')
