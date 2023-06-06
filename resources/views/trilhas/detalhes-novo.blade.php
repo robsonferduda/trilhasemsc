6d18cdd6e33e0248612862104fac81d7c5e84b20
Robson <robson@coperve.ufsc.br>
2023-06-06 15:00:54 -0300

    Layout Detalhes Trilhas

--- resources/views/trilhas/detalhes-novo.blade.php
+++ resources/views/trilhas/detalhes-novo.blade.php
@@ -10,23 +10,9 @@
 <section class="pt-1 pb-0 mt-3 mb-5">
     <div class="container">
         <div class="row">
-            <div class="col-md-9 mt-2">
+            <div class="col-lg-12 col-md-12 col-sm-12 mt-2">
                 <div class="col-md-12">
                     <a href="#"><img class="border-radius-xl shadow w-100" src="{{ asset('img/trilhas/detalhes-principal/'.$img) }}" alt="{{ $alt }}"></a>
-                </div>
-            </div>
-            <div class="col-md-3 mt-2">
-                <div class="col-lg-12 col-md-12 position-relative my-auto justify-content-center text-center">
-                    <img class="mx-auto" src="{{ asset('img/trilhas/nivel/'.$trilha->nivel->dc_icone_niv) }}" alt="Grau de dificuldade da trilha {{ $trilha->nivel->dc_nivel_niv }}">
-                    <span style="color: white; background: {{ ($trilha->nivel) ? $trilha->nivel->dc_color_nivel_niv : '#989898' }};" class="badge badge-secondary">{{ $trilha->nivel->dc_nivel_niv }} {{ ($trilha->complemento) ? " - ".$trilha->complemento->nm_complemento_nivel_con : '' }}</span>
-                </div>
-                <div class="col-lg-12 col-md-12 mt-5">
-                    <p><i class="fa fa-road text-success"></i> <strong>Distância</strong>: </p>
-                    <p><i class="fa fa-clock-o text-info"></i> <strong>Tempo Médio</strong>: </p>
-                    <p><i class="fa fa-sun text-warning"></i> <strong>Exposição</strong>: </p>
-                    <p><i class="fa fa-heartbeat text-danger"></i> <strong>Esforço</strong>: </p>
-                    <p><i class="fa fa-compass text-info"></i> <strong>Orientação</strong>: </p>
-                    <p><a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">Clique e entenda as medidas</a></p>
                 </div>
             </div>
         </div>
@@ -56,6 +42,22 @@
                     </div>
                 </div> 
                 <div class="col-md-12 mt-2">
+                    <div class="col-lg-3 col-md-3 col-sm-12 position-relative my-auto justify-content-center text-center">
+                        <img class="mx-auto" src="{{ asset('img/trilhas/nivel/'.$trilha->nivel->dc_icone_niv) }}" alt="Grau de dificuldade da trilha {{ $trilha->nivel->dc_nivel_niv }}">
+                        <span style="color: white; background: {{ ($trilha->nivel) ? $trilha->nivel->dc_color_nivel_niv : '#989898' }};" class="badge badge-secondary">{{ $trilha->nivel->dc_nivel_niv }} {{ ($trilha->complemento) ? " - ".$trilha->complemento->nm_complemento_nivel_con : '' }}</span>
+                    </div>
+                    <!--
+                        <div class="col-lg-12 col-md-12 mt-5">
+                            <p><i class="fa fa-road text-success"></i> <strong>Distância</strong>: </p>
+                            <p><i class="fa fa-clock-o text-info"></i> <strong>Tempo Médio</strong>: </p>
+                            <p><i class="fa fa-sun text-warning"></i> <strong>Exposição</strong>: </p>
+                            <p><i class="fa fa-heartbeat text-danger"></i> <strong>Esforço</strong>: </p>
+                            <p><i class="fa fa-compass text-info"></i> <strong>Orientação</strong>: </p>
+                            <p><a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">Clique e entenda as medidas</a></p>
+                        </div>
+                    -->
+                </div>
+                <div class="col-md-12 mt-2">
                     <h4 class="text-success">
                         Use o Wikiloc
                     </h4>
@@ -65,20 +67,17 @@
                 </div>
             </div>
             <div class="col-md-3">            
-                <div class="col-lg-12 mt-3">
+                <div class="col-lg-12 mt-4">
                     <h4 class="mt-0">Busca por Cidade</h4>
-                    @foreach($busca_cidade as $busca)
-                        <a href="{{url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista')}}">
-                            <div class="card justify-content-center mb-3">
-                            <div class="card-body p-3">
-                            <h6 class="mb-0">{{ $busca->cidade->nm_cidade_cde }} ({{ $busca->total }})</h6>
-                            </div>
-                            <div class="position-absolute end-0 me-3 ">
-                            <i class="fas fa-angle-right text-dark" aria-hidden="true"></i>
-                            </div>
-                            </div>
-                        </a>
-                    @endforeach
+                        <ul class="flex-column ms-n3 nav">
+                            @foreach($busca_cidade as $busca)
+                                <li class="nav-item">
+                                    <a class="nav-link text-secondary" href="{{url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista')}}">
+                                        <h6>{{ $busca->cidade->nm_cidade_cde }} ({{ $busca->total }})</h6>
+                                    </a>
+                                </li>
+                            @endforeach
+                      </ul>
                 </div>
             </div>         
        </div>
