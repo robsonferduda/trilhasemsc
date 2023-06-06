a81065bac6a5712414ee93f3ddbc8282ffad94b0
Robson <robson@coperve.ufsc.br>
2023-06-06 16:59:31 -0300

    Menu

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -27,6 +27,13 @@
    <body class="help-center">
       <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
          <div class="container">           
+            <button class="navbar-toggler shadow-none ms-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon mt-2">
+               <span class="navbar-toggler-bar bar1"></span>
+               <span class="navbar-toggler-bar bar2"></span>
+               <span class="navbar-toggler-bar bar3"></span>
+               </span>
+            </button>
             <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
                <ul class="navbar-nav navbar-nav-hover mx-auto">
                 <li class="nav-item dropdown dropdown-hover mx-2">
