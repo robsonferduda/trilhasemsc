cfab92981dd6bb742bd03af6e849bac9fa2337b7
Robson <robson@coperve.ufsc.br>
2023-06-06 11:47:23 -0300

    Alterações Site

--- resources/views/layouts/partes/header.blade.php
+++ resources/views/layouts/partes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
        <div class="row bg-white shadow mt-n5 border-radius-lg pb-4 p-3 mx-sm-0 mx-1 position-relative">
           <form action="{{ url('trilhas/#lista') }}" style="display: inherit;" method="GET" id="banner-searchbox" class="">
-            <div class="row">
+            <div class="row w-100">
                <div class="col-lg-4 col-md-4 col-sm-12 mt-2 mr-2" style="padding-right: 15px;">
                   <label class="">Selecione a cidade</label>
                   <select class="form-control" name="cidade" id="list-cidade">
@@ -27,13 +27,13 @@
                      @endif
                   </select>
                </div>
-               <div class="col-lg-3 col-lg-3 col-sm-12 mt-2 mr-2" style="padding-right: 15px;">
+               <div class="col-lg-3 col-md-3 col-sm-12 mt-2 mr-2" style="padding-right: 15px;">
                   <label class="">Nome da Trilha</label>
                   <div class="input-group">
                      <input type="text" name="termo" class="form-control flatpickr-input" placeholder="Nome da Trilha">
                   </div>
                </div>
-               <div class="col-lg-2 mt-2">
+               <div class="col-lg-2 col-md-2 col-sm-12 mt-2">
                   <label class="">&nbsp;</label>
                   <button type="submit" class="btn bg-gradient-primary w-100 mb-0">Buscar Trilhas</button>
                </div>
