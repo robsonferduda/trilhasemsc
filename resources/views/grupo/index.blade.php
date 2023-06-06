035f3a59372029e6836591ff7c2d7dd040778473
Robson <robson@coperve.ufsc.br>
2023-06-06 16:27:28 -0300

    Página de Grupos

--- resources/views/grupo/index.blade.php
+++ resources/views/grupo/index.blade.php
@@ -1,58 +1,50 @@
-@extends('layouts.blog')
+@extends('layouts.site')
 @section('content')
-        <div class="blog-post-area section-padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="section-title text-center">
-                            <div class="title-border">
-                                <br/><br/><h1>Grupos</h1>
-                            </div>    
-                        </div>
-                    </div>                          
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('img/grupos/familia_na_trilha.png') }}" alt="Logo Família na Trilha">
-                    </div>
-                    <div class="col-md-9">
-                        <h3>Família na Trilha</h3>
-                        <h4>Operadora de turismo de aventura</h4>
-                        <p><strong>Cidade</strong>: Florianópolis</p>
-                        <p><strong>Responsável</strong>: Claudia Tiscoski</p>
-                        <p><strong>Contato</strong>: (48) 4141-0450</p>
-                        <a class="btn btn-success" href="https://chat.whatsapp.com/GvvzWUySo9f00HGikK11kP"><i class="fa fa-whatsapp" aria-hidden="true"></i> Entrar no Grupo</a>
-                    </div>
-                </div>
-                <hr/>
-                <div class="row">
-                    <div class="col-md-3">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('img/grupos/litoral_trilhas.jpg') }}" alt="Logo Família na Trilha">
-                    </div>
-                    <div class="col-md-9">
-                        <h3>Litoral Trilhas</h3>
-                        <h4>Trilhas, Praias e Acampamentos</h4>
-                        <p><strong>Cidade</strong>: Brusque</p>
-                        <p><strong>Responsável</strong>: Rachel e Vilson </p>
-                        <p><strong>Contato</strong>: (47) 98870-6304</p>
-                        <a class="btn btn-success" href="https://chat.whatsapp.com/CFb4KMQ864NGhPZc0WmeKV"><i class="fa fa-whatsapp" aria-hidden="true"></i> Entrar no Grupo</a>
-                    </div>
-                </div>
-                <hr/>
-                <!--
-                <div class="row">
-                    <div class="col-md-3">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('img/grupos/vibe_de_aventura.jpg') }}" alt="Logo Família na Trilha">
-                    </div>
-                    <div class="col-md-9">
-                        <h3>Vibe de Aventura</h3>
-                        <h4>A vida é uma estrada, aproveite o caminho!</h4>
-                        <p><strong>Cidade</strong>: Palhoça</p>
-                        <p><strong>Responsável</strong>: Leonardo</p>
-                        <p><strong>Contato</strong>: (48) 98433-0946 </p>
-                    </div>
-                </div>
-            -->
+    @include('layouts/partes/header')
+    <div class="container">
+        <div class="row mb-2 mt-3">
+            <div class="col-md-7">
+               <h3 class="text-primary">Grupos de Whatsapp</h3>
+               <p>Os grupos, mensagens e conteúdos são de responsabilidade de seus idealizadores.</p>
             </div>
+         </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+               <div class="card card-profile" style="min-height: 320px;">
+                 
+                 <div class="card-body justify-content-center text-center">
+                   <a href="javascript:;">
+                       <img class="avatar avatar-xxl shadow-lg rounded-circle mx-auto" src="{{ asset('img/grupos/familia_na_trilha.png') }}">
+                     </a>
+                   <h6 class="mb-0 mt-2">Família na Trilha</h6>
+                   <p>Operadora de turismo de aventura</p>
+                   <p><strong>Cidade</strong>: Florianópolis</p>
+                   <p><strong>Responsável</strong>: Claudia Tiscoski</p>
+                   <p><strong>Contato</strong>: (48) 4141-0450</p>
+                   <a class="btn btn-success" href="https://chat.whatsapp.com/GvvzWUySo9f00HGikK11kP"><i class="fa fa-whatsapp" aria-hidden="true"></i> Entrar no Grupo</a>
+                 </div>
+               </div>
+            </div>   
+            
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-profile" style="min-height: 320px;">
+                  
+                  <div class="card-body justify-content-center text-center">
+                    <a href="javascript:;">
+                        <img class="avatar avatar-xxl shadow-lg rounded-circle mx-auto" src="{{ asset('img/grupos/litoral_trilhas.jpg') }}">
+                      </a>
+                    <h6 class="mb-0 mt-2">Litoral Trilhas</h6>
+                    <p>Trilhas, Praias e Acampamentos</p>
+                    <p><strong>Cidade</strong>: Brusque</p>
+                    <p><strong>Responsável</strong>: Rachel e Vilson </p>
+                    <p><strong>Contato</strong>: (47) 98870-6304</p>
+                    <a class="btn btn-success" href="https://chat.whatsapp.com/CFb4KMQ864NGhPZc0WmeKV"><i class="fa fa-whatsapp" aria-hidden="true"></i> Entrar no Grupo</a>
+                  </div>
+                </div>
+             </div> 
+             <div class="col-lg-12 col-md-12 mt-4 text-center">
+                <a href="{{ url('/') }}" type="button" class="btn btn-outline-warning btn-sm">Voltar ao Início</a>
+              </div>
         </div>
+     </div>
 @endsection
