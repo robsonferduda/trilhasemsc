e599c36a3773c5dc38ce0cc7e64dd4ffc0b2fbab
Robson <robson@coperve.ufsc.br>
2023-06-21 17:00:14 -0300

    Login

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -63,3 +63,35 @@
     </div>
 </div>
 @endsection
+
+@extends('layouts.site')
+@section('pageTitle','Cadastro de Guias e Condutores')
+@section('description', 'Guia de trilhas e camping em Santa Catarina, trazendo informações de localização, trajetos e grau de dificuldade para quem quer conhecer e desfrutar das praias, serras e montanhas desse belo estado do Sul do Brasil' )
+
+@section('content')
+<div class="row mt-n8 mb-5">
+    <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+        <div class="card z-index-0">
+           <div class="card-header text-center pt-4 mt-4">
+              <h5>Recuperar Senha</h5>
+           </div>
+           <div class="card-body">
+               @include('layouts/mensagens-novo')
+                <form action="{{ route('login') }}" method="post" >
+                    @csrf
+                    <div class="mb-3">                    
+                        <input name="email" id="email" type="email" class="form-control @error('name') is-invalid @enderror" value="{{ old('email') }}" placeholder="Email" required autocomplete="email" autofocus>
+                    </div>
+                    <div class="mb-3">    
+                        <input name="password" id="password" type="password" class="form-control required @error('password') is-invalid @enderror" placeholder="Senha" required autocomplete="current-password">                
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn bg-gradient-dark w-40 my-4 mb-2">Login</button>
+                    </div>
+                 <p class="text-sm mt-3 mb-0 text-center">Esqueceu sua senha? <br/><a href="{{ route('password.request') }}" class="text-dark font-weight-bold text-decoration-underline-hover">Clique aqui</a></p>
+                </form>
+           </div>
+        </div>
+     </div>
+</div>
+@endsection
--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -23,8 +23,18 @@
          }
       </style>
       {!! htmlScriptTagJsApi() !!}
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5H3D7W9');</script>
+      <!-- End Google Tag Manager -->
    </head>
    <body class="help-center">
+      <!-- Google Tag Manager (noscript) -->
+         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H3D7W9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
       <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
          <div class="container">           
             <button class="navbar-toggler shadow-none ms-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
@@ -69,6 +79,7 @@
                </ul>
                <ul class="navbar-nav d-lg-block d-none">
                   <li class="nav-item">
+                     <a href="{{ url('login') }}" class="btn btn-sm  bg-gradient-info  btn-round mb-0 me-1">LOGIN</a>
                      <!--
                         <a href="{{ url('guias-e-condutores') }}" class="btn btn-sm  bg-gradient-primary  btn-round mb-0 me-1">TRILHEIROS</a>
                         <a href="{{ url('guias-e-condutores/cadastro') }}" class="btn btn-sm  bg-gradient-warning  btn-round mb-0 me-1">CONDUTORES</a>
