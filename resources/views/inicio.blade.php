268a5a8a112253d7661f94183c1b61362de3cd38
Robson Fernando Duda <robsonferduda@gmail.com>
2024-08-31 18:59:25 -0300

    Publicidade

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,45 +3,10 @@
     @include('layouts/partes/header')
     @include('layouts/partes/acesso_guias')
     @include('inicio/destaques')
-    <section class="pt-1 pb-0 mt-0 mb-5">
-        <div class="container">
-           <div class="row mb-2">
-              <div class="col-md-7">
-                 <h3 class="text-default">Publicidade</h3>
-              </div>
-           </div>
-           <div class="row">
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-1229685353625953"
-                    data-ad-slot="7739149091"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true">
-                </ins>
-                <script> (adsbygoogle = window.adsbygoogle || []).push({});  </script>  
-            </div>
-        </div>
-    </section>
     @include('inicio/eventos')
+    @include('inicio/publicidade')
     @include('inicio/condutores')
-    <section class="pt-1 pb-0 mt-0 mb-5">
-        <div class="container">
-           <div class="row mb-2">
-              <div class="col-md-7">
-                 <h3 class="text-default">Publicidade</h3>
-              </div>
-           </div>
-           <div class="row">
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-1229685353625953"
-                    data-ad-slot="7739149091"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true">
-                </ins>
-                <script> (adsbygoogle = window.adsbygoogle || []).push({});  </script>  
-            </div>
-        </div>
-    </section>
+    @include('inicio/publicidade')
     @include('inicio/favoritos')
+    @include('inicio/publicidade')
 @endsection
---
+++ resources/views/layouts/partes/publicidade.blade.php
@@ -0,0 +1,19 @@
+<section class="pt-1 pb-0 mt-0 mb-5">
+    <div class="container">
+       <div class="row mb-2">
+          <div class="col-md-7">
+             <h3 class="text-default">Publicidade</h3>
+          </div>
+       </div>
+       <div class="row box-publicidade">
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-1229685353625953"
+                data-ad-slot="7739149091"
+                data-ad-format="auto"
+                data-full-width-responsive="true">
+            </ins>
+            <script> (adsbygoogle = window.adsbygoogle || []).push({});  </script>  
+        </div>
+    </div>
+</section>
