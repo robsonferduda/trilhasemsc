4acd56bdab472f40aa4d97a66e95ced79a80ffc9
Robson Fernando Duda <robsonferduda@gmail.com>
2024-09-15 16:26:39 -0300

    Perfil

--- resources/views/guias/perfil.blade.php
+++ resources/views/guias/perfil.blade.php
@@ -26,7 +26,7 @@
          <div class="col-md-12 mb-3 mt-3">
             <div class="row">
                @forelse ($guia->unidadesConservacao as $key => $uc)
-                  <div class="col col-xl-2 col-md-2 mb-3 mt-3">
+                  <div class="col col-xl-2 col-md-2 mb-3 mt-3 center">
                      <p class="center"><strong>{{ Str::upper($uc->sigla_unc) }}</strong></p>
                      <img class="img-fluid w-75" src=" {{ asset('img/logos-uc/'.$uc->logo_unc) }}" alt="Logo {{ $uc->nome_uc }}">
                   </div>
@@ -35,7 +35,7 @@
                @endforelse
             </div>
          </div>
-         <div class="col-md-12 mt-2">
+         <div class="col-md-12 mt-2 center">
             <a class="btn btn-success" href="{{ url("guia/perfil/estatistica/telefone", $guia->nm_instagram_gui) }}"><i class="fa fa-whatsapp" aria-hidden="true"></i> Enviar Mensagem</a>
             <a class="btn btn-primary mr-2 ml-3" href="{{ url("guias-e-condutores") }}"><i class="fa fa-users" aria-hidden="true"></i> Todos os Guias</a>
          </div> 
