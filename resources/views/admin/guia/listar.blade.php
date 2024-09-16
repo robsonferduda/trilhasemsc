40e9bc5c3b7db67006ef9f7610553a9015821234
Robson Fernando Duda <robsonferduda@gmail.com>
2024-09-16 10:24:06 -0300

    Score

--- resources/views/admin/guia/listar.blade.php
+++ resources/views/admin/guia/listar.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <div class="card">
                         <div class="row p-5">
-                            <div class="col-lg-2 col-md-2 col-sm-12">
+                            <div class="col-lg-1 col-md-1 col-sm-12">
                                 <img src="{{ $guia->nm_path_logo_gui ? asset('img/guias/'.$guia->nm_path_logo_gui) : asset('images/user.png') }}" class="rounded-circle user-photo w-100" alt="Foto de Perfil">
                             </div>
                             <div class="col-lg-10 col-md-10 col-sm-12">
@@ -30,7 +30,6 @@
                                         <p><i class="fa fa-instagram"></i> <a href="{{ url("guia/perfil/estatistica/instagram", $guia->id_guia_gui) }}">{{ $guia->nm_instagram_gui }}</a></p>
                                         <p><strong>Cidades de Origem</strong>: {{ ($guia->origem) ? $guia->origem->nm_cidade_cde : 'Santa Catarina' }}</p>
                                         <p><strong>Cidades de Atuação</strong>: {{ count($guia->cidadesAtuacao) > 0 ? implode(', ',$guia->cidadesAtuacao->pluck('nm_cidade_cde')->toArray()) : $guia->ds_atuacao_gui }}</p>
-                                        <p><strong>Bibliografia</strong>{{ $guia->dc_biografia_gui }}</p>
                                     </div>
                                     <div class="col-lg-3 col-md-3 col-sm-12">
                                         <div style="position: absolute; right: 0;">
@@ -41,6 +40,7 @@
                                                 <a href="{{ url('guia/'.$guia->id_guia_gui.'/ativar') }}" class="btn btn-success mr-1 mt-1"><i class="fa fa-check-circle"></i> Ativar</a>
                                                 <a href="#" class="btn btn-danger disabled mr-1 mt-1"><i class="fa fa-ban"></i> Recusar</a>
                                             @endif
+                                            <a href="{{ url('guia/perfil/estatistica/perfil/'.$guia->id_guia_gui) }}" class="btn btn-success disabled mr-1 mt-1"><i class="fa fa-check-circle"></i> Perfil</a>
                                         </div>
                                     </div>
                                 </div>
