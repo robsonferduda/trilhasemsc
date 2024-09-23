780c371ced0a40d45b544fcb5a76209b3f1af3e7
Robson Fernando Duda <robsonferduda@gmail.com>
2024-09-23 08:49:16 -0300

    Foto

--- resources/views/trilheiro/perfil.blade.php
+++ resources/views/trilheiro/perfil.blade.php
@@ -32,7 +32,7 @@
                                     <div class="profile-image center mb-4">
                                         @switch(trim(Auth::user()->id_role))
                                             @case('ADMIN')
-                                                <img src="{{ $trilheiro->dc_foto_perfil ? asset('img/guias/'.$trilheiro->dc_foto_perfil) : asset('images/user.png') }}" class="rounded-circle w-50 " alt="Foto de Perfil">
+                                                <img src="{{ $trilheiro->nm_path_foto_tri ? asset('img/guias/'.$trilheiro->nm_path_foto_tri) : asset('images/user.png') }}" class="rounded-circle w-50 " alt="Foto de Perfil">
                                                 @break
                                             @case('GUIA')
                                                 <img src="{{ Auth::user()->dc_foto_perfil ? asset('img/guias/'.Auth::user()->dc_foto_perfil) : asset('images/user.png') }}" class="rounded-circle w-50 " alt="Foto de Perfil">
