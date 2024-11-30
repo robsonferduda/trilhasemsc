1d6c66138588bd8305ed536b38e0df7c9cc4de12
Robson Fernando Duda <robsonferduda@gmail.com>
2024-11-30 10:21:10 -0300

    Trilhas em SC

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -29,7 +29,7 @@
                         <p class="mb-1"><strong>Data/Horário Início</strong>: {{ \Carbon\Carbon::parse($evento->dt_realizacao_eve)->format('d/m/Y')}} - {{ \Carbon\Carbon::parse($evento->hora_inicio_eve)->format('H:i') }}</p>
                         <p class="mb-1"><strong>Data/Horário Término</strong>: {{ \Carbon\Carbon::parse($evento->dt_termino_eve)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($evento->hora_fim_eve)->format('H:i') }}</p>
                         <p class="mb-1"><strong>Valor</strong>: R$ {{ $evento->valor_eve }}</p>
-                        <p class="mb-1"><strong>Contato</strong>: {{ $evento->local->nm_cidade_cde }}</p>
+                        <p class="mb-1"><strong>Contato</strong>: {{ $evento->ds_fone_contato_eve }}</p>
                     </div>
 
                     <div class="col-lg-2 col-md-2 position-relative text-center d-none d-md-block">
@@ -39,7 +39,7 @@
                         <img class="avatar avatar-xxl shadow-lg rounded-circle mx-auto" src="{{ asset('img/guias/default.png') }}" alt="Logo Guia {{ $evento->guia->nm_guia_gui }}">
                         @endif
                     </div>
-                    
+
                     <div class="col-lg-12 col-md-12">
                         <div class="center">
                             <a href="{{ url('eventos/detalhes', $evento->id_evento_eve) }}" type="button" class="btn btn-outline-info btn-sm">Ver Detalhes</a>
