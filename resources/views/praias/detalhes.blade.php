1a88f7de07741f6a25a531571298641cf1b506af
Robson Fernando Duda <robsonferduda@gmail.com>
2025-02-19 12:19:50 -0300

    Praias

--- resources/views/praias/detalhes.blade.php
+++ resources/views/praias/detalhes.blade.php
@@ -8,37 +8,26 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 mt-2">
                 <div class="col-md-12">
-                    <a href="#"><img class="border-radius-xl shadow w-100" src="{{ asset('img/trilhas/detalhes-principal/'.$img) }}" alt="{{ $alt }}"></a>
+                    <a href="#"><img class="border-radius-xl shadow w-100" src="{{ asset('img/trilhas/detalhes-principal/padrao.jpg') }}" alt=""></a>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-9 mt-2">
                 <div class="col-md-12 mt-3">
-                    <h4 class="mt-2">{{ $trilha->nm_trilha_tri }}</h4>
+                    <h4 class="mt-2">{{ $praia->nm_praia_pra }}</h4>
                     <h6>
-                        <i class="ni ni-pin-3 text-danger"></i> {{ $trilha->cidade->nm_cidade_cde }} 
+                        <i class="ni ni-pin-3 text-danger"></i> Florianópolis 
                         <span class=""><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK" style="color: #e73177;"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></span>
-                    </h6>
-                    <p class="mb-1">
-                        <span class="text-info">Trilha cadastrada em {{ \Carbon\Carbon::parse($trilha->created_at)->format('d/m/Y') }} com última atualização em {{ \Carbon\Carbon::parse($trilha->updated_at)->format('d/m/Y') }}</span>
-                    </p>
-                    <p class="mb-1">
-                        (Fique sempre atento à data de atualização dos textos. Eles são feitos com base em nossas trilhas e podem estar desatualizados em razão do tempo da última visita.)
-                    </p>                    
+                    </h6>               
                 </div> 
                 <div class="col-md-12 mt-3">
                     <div class="mt-0 conteudo">
-                        {!! $trilha->txt_telegram_pra !!}
+                        {!! $praia->txt_telegram_pra !!}
                     </div>
                 </div>
                 <div class="col-md-12">
                     <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-12 position-relative my-auto justify-content-center text-center">
-                            <img class="mx-auto" src="{{ asset('img/trilhas/nivel/'.$trilha->nivel->dc_icone_niv) }}" alt="Grau de dificuldade da trilha {{ $trilha->nivel->dc_nivel_niv }}">
-                            <span style="color: white; background: {{ ($trilha->nivel) ? $trilha->nivel->dc_color_nivel_niv : '#989898' }};" class="badge badge-secondary">{{ $trilha->nivel->dc_nivel_niv }} {{ ($trilha->complemento) ? " - ".$trilha->complemento->nm_complemento_nivel_con : '' }}</span>
-                            <p class="mt-3"><a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">Clique e entenda as medidas</a></p>
-                        </div>
                         <div class="col-lg-4 col-md-4 col-sm-12" style="text-align: center">
                             <h5 class="mb-2 mt-2">Gostou do conteúdo?</h5>                          
                             <p class="mb-2">Que tal nos pagar um café?</p>
