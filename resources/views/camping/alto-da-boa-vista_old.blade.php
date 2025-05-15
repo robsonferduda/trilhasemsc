9506e585d4b5c90f28c1fec5718329a4b3e5c529
Robson <robson@coperve.ufsc.br>
2025-05-15 10:13:37 -0300

    Camping

--- resources/views/camping/alto-da-boa-vista_old.blade.php
+++ resources/views/camping/alto-da-boa-vista_old.blade.php
@@ -1,42 +1,38 @@
-@extends('layouts.site')
+@extends('layouts.blog')
+
 @section('pageTitle', 'Camping Mirante do Alto da Boa Vista' )
-@section('description', "")
+
+@section('description', 'Localizado no munícipio catarinense de Rancho Queimado, o Mirante Alto da Boa Vista é uma ótima opção de camping' )
+
 @section('content')
-<header class="position-relative">
-    <div class="container">
-        <div class="row bg-white shadow mt-n5 border-radius-lg pb-3 p-3 mx-sm-0 mx-1 position-relative">
-            <div class="col-md-12 z-index-2 position-relative px-md-2 px-sm-5 mt-sm-0 mt-4 text-center">
-                <h2 class="mt-2">Camping Mirante do Alto da Boa Vista</h2>
-                <h5><i class="ni ni-pin-3 text-danger mb-1"></i> Rancho Queimado</h5>
-            </div>
-       </div>
-    </div>
- </header>
-<section class="pt-1 pb-0 mt-3 mb-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 mt-2">
-                <div class="col-md-12">
-                    <a href="#"><img class="border-radius-xl shadow w-100" src="http://trilhasemsc.localhost/public/img/camping/alto-da-boa-vista/capa.jpg" alt=""></a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-9 mt-2">
-                <div class="col-md-12 mt-3">
-                    <h4 class="mt-2">Camping Mirante do Alto da Boa Vista</h4>
-                    <h6>
-                        <i class="ni ni-pin-3 text-danger"></i> Rancho Queimado 
-                        <span class=""><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK" style="color: #e73177;"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></span>
-                    </h6>
-                    <p class="mb-1">
-                        (Fique sempre atento à data de atualização dos textos. Eles são feitos com base em nossas trilhas e podem estar desatualizados em razão do tempo da última visita.)
-                    </p>                    
-                </div> 
-               
-                <div class="col-md-12 mt-3">
-                    <div class="mt-0 conteudo">
-                         <p>
+        <div class="blog-post-area section-padding">
+            <div class="container">                
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="single-blog-post blog-post-details">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="{{ asset('img/camping/alto-da-boa-vista/capa.jpg') }}" alt="Vista panorâmica do Mirante da Boa Vista"></a>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4>Camping Mirante do Alto da Boa Vista</h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>Robson Fernando Duda</span>
+                                </div>
+                                <!-- Horizontal Tela Detalhes Trilha -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-1229685353625953"
+                                     data-ad-slot="7739149091"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                     (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                                <br />
+                                <p>
+                                    <p class="text-danger"><i class="fa fa-ban" aria-hidden="true"></i> Não jogue lixo na natureza</p>
+                                </p>
+                                <p>
                                     Se você está na região da Grande Florianópolis e procura um lugar bonito para acampar, mas que não exija muito esforço físico, encontrou uma ótima opção. O Mirante do Alto da Boa Vista fica localizado no município de Rancho Queimado, conhecido como a Capital Catarinense do Morango.
                                     Distante 60km de Florianópolis, a pequena cidade de colonização alemã é muito procurada pelas suas fazendas e pousadas. 
                                 </p>
@@ -119,92 +115,49 @@
                                     <img src="{{ asset('img/camping/alto-da-boa-vista/fim_de_tarde.jpg') }}" alt="Pôr do Sol no Mirante Alto da Boa Vista">
                                     <legend class="border-none"><strong>Foto</strong>: Robson Fernando Duda</legend>
                                 </div>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-12" style="text-align: center">
-                            <h5 class="mb-2 mt-2">Gostou do conteúdo?</h5>                          
-                            <p class="mb-2">Que tal nos pagar um café?</p>
-                            <a href="https://pag.ae/7-dSqqaS8" class="btn btn-sm bg-gradient-info btn-round mb-0 me-1"><i class="fa fa-coffee" style="font-size: 12px;" aria-hidden="true"></i> Pagar um café!</a>                            
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12" style="text-align: center">
-                            <h5 class="mb-2 mt-2">Também pode ser um PIX</h5>   
-                            <p class="mb-2">Chave: trilhasemsc@gmail.com</p>     
-                            <img class="mx-auto" src="{{ asset('img/qrcode.png') }}" style="width: 80%;" alt="PIX TrilhasemSC">                                              
-                        </div>
-                        <!--
-                            <div class="col-lg-12 col-md-12 mt-5">
-                                <p><i class="fa fa-road text-success"></i> <strong>Distância</strong>: </p>
-                                <p><i class="fa fa-clock-o text-info"></i> <strong>Duração</strong>: </p>
-                                <p><i class="fa fa-sun text-warning"></i> <strong>Exposição</strong>: </p>
-                                <p><i class="fa fa-heartbeat text-danger"></i> <strong>Esforço</strong>: </p>
-                                <p><i class="fa fa-compass text-info"></i> <strong>Orientação</strong>: </p>
-                                <p><a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">Clique e entenda as medidas</a></p>
+                               
+                                <h5><strong>Texto</strong>: Robson Fernando Duda</h5>
                             </div>
-                        -->
-                    </div>
-                </div>
-               
-                @include('layouts/partes/publicidade-google')
-                <div class="row mb-3">
-                    <div class="col-lg-12 col-sm-12 mb-sm-0 mb-4">
-                       <div class="info-horizontal bg-gradient-primary border-radius-lg p-3">
-                          <i class="ni ni-calendar-grid-58 h4 text-white icon"></i>
-                          <div class="description ps-3">
-                             <h5 class="text-white">Atualização das Informações</h5>
-                             <p class="text-white">Fique sempre atento à data de atualização dos textos. Os textos são feitos com base em nossas trilhas e podem estar desatualizados em razão do tempo da última visita.</p>
-                          </div>
-                       </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12 col-sm-12 mb-sm-0 mb-4">
-                        <div class="info-horizontal bg-gradient-info border-radius-lg p-3">
-                           <i class="ni ni-compass-04 h4 text-white icon"></i>
-                           <div class="description ps-3">
-                              <h5 class="text-white">Contrate um Condutor</h5>
-                              <p class="text-white">Sempre que visitar um local desconhecido, procure por condutores locais autorizados. Faça turismo seguro.</p>
-                              <a href="{{ url('guias-e-condutores') }}">Lista de Condutores</a>
-                           </div>
+                            <h4 class="center"><a class="link" href="{{ url('/') }}">Voltar para o início</a></h4>
+                            <!-- Horizontal Tela Detalhes Trilha -->
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-client="ca-pub-1229685353625953"
+                                 data-ad-slot="7739149091"
+                                 data-ad-format="auto"
+                                 data-full-width-responsive="true"></ins>
+                            <script>
+                                 (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>                            
                         </div>
-                     </div>
-                 </div>    
-            </div>
-            <div class="col-md-3">            
-                <div class="col-lg-12 mt-4">
-                    <h4 class="mt-0">Busca por Cidade</h4>                    
-                        <label class="" style="font-size: 100%; margin: 0px;">Selecione a cidade</label>
-                        <p class="mt-0 text-danger" style="font-size: 85%;">São mostradas somente cidades que possuem alguma trilha cadastrada.</p>
-                        <select class="form-control mb-0" name="cidade" id="list-cidade">
-                           <option value="">Selecione a cidade</option>
-                           @if(isset($busca_cidade))
-                              @forelse($busca_cidade as $busca)
-                                 <option value="{{ url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista') }}">{{ $busca->cidade->nm_cidade_cde }} ({{ $busca->total }})</option>
-                              @empty
-                                 <option selected value="">Nenhuma cidade disponível</option>
-                              @endforelse
-                           @endif
-                        </select>
-                        @include('layouts/partes/publicidade-google')
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sidebar-widget">
+                            <div class="single-sidebar-widget">
+                                <h4>PESQUISAR <span>Trilha</span></h4>
+                                <form id="text-search" action="{{url('trilhas/#lista')}}" >
+                                    <input type="text" name="termo" placeholder="Digite aqui">
+                                    <button class="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="single-sidebar-widget country-select">
+                                <h4>BUSCA POR <span>Cidade</span></h4>
+                                <ul class="widget-categories">
+                                    @foreach($busca_cidade as $busca)
+                                        <li><a href="{{url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista')}}">{{ $busca->cidade->nm_cidade_cde }}<span>({{ $busca->total }})</span></a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+    
+                            <div class="col-lg-12 col-md-12 col-sm-12" style="min-height: 570px; background: #f1f1f1;display: none">
+                                <div class="box-publicidade-detalhes">
+                                    <span>PUBLICIDADE</span>
+                                </div>
+                            </div>                                
+                        </div>
+                    </div>
                 </div>
-            </div>         
-       </div>
-       <div class="row">
-            <div class="col-md-9 mt-2">
-            <!-- @env('production')
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-1229685353625953"
-                    data-ad-slot="7739149091"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>                
-            @endenv -->
             </div>
-       </div>
-    </div>
- </section>
+        </div>
 @endsection
