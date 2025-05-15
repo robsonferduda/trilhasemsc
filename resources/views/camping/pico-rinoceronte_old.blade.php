55024b9d87e3e1de03b4fdfdffcc61f9282b1cd6
Robson <robson@coperve.ufsc.br>
2025-05-15 10:22:50 -0300

    Camping

--- resources/views/camping/pico-rinoceronte_old.blade.php
+++ resources/views/camping/pico-rinoceronte_old.blade.php
@@ -1,44 +1,39 @@
-@extends('layouts.site')
+@extends('layouts.blog')
+
 @section('pageTitle', 'Camping Pico do Rinoceronte' )
-@section('description', "")
+
+@section('description', 'Entre as belezas de Bom Jardim da Serra está o Pico do Rinoceronte, um lugar com paisagem única com uma belíssima vista dos cânions da região e também das cidades de Siderópolis e Criciúma' )
+
 @section('content')
-<header class="position-relative">
-    <div class="container">
-        <div class="row bg-white shadow mt-n5 border-radius-lg pb-3 p-3 mx-sm-0 mx-1 position-relative">
-            <div class="col-md-12 z-index-2 position-relative px-md-2 px-sm-5 mt-sm-0 mt-4 text-center">
-                <h2 class="mt-2">Camping Pico do Rinoceronte</h2>
-                <h5><i class="ni ni-pin-3 text-danger mb-1"></i> Bom Jardim da Serra</h5>
-            </div>
-       </div>
-    </div>
- </header>
-<section class="pt-1 pb-0 mt-3 mb-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 mt-2">
-                <div class="col-md-12">
-                    <a href="#"><img class="border-radius-xl shadow w-100" src="https://trilhasemsc.com.br/public/img/trilhas/detalhes-principal/trilha-do-pico-do-rinoceronte.jpg" alt=""></a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-9 mt-2">
-                <div class="col-md-12 mt-3">
-                    <h4 class="mt-2">Camping Pico do Rinoceronte</h4>
-                    <h6>
-                        <i class="ni ni-pin-3 text-danger"></i> Bom Jardim da Serra 
-                        <span class=""><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK" style="color: #e73177;"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></span>
-                    </h6>
-                    <p class="mb-1">
-                        (Fique sempre atento à data de atualização dos textos. Eles são feitos com base em nossas experiências e podem estar desatualizados em razão do tempo da última visita.)
-                    </p>                    
-                </div> 
-               
-                <div class="col-md-12 mt-3">
-                    <p class="text-info mb-1"><i class="fa fa-info" aria-hidden="true"></i> Última atualização em 27 de agosto de 2022</p>
-                    <p class="text-danger mb-1"><i class="fa fa-trash" aria-hidden="true"></i> Não jogue lixo</p>
-                    <p class="text-success"><i class="fa fa-leaf" aria-hidden="true"></i> Proteja a natureza e os animais selvagens</p>
-                    <div class="mt-0 conteudo">
+        <div class="blog-post-area section-padding">
+            <div class="container">                
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="single-blog-post blog-post-details">
+                            <div class="single-blog-post-img">
+                                <a href="#"><img src="https://trilhasemsc.com.br/public/img/trilhas/detalhes-principal/trilha-do-pico-do-rinoceronte.jpg" alt="Vista panorâmica Pico do Rinoceronte"></a>
+                            </div>
+                            <div class="single-blog-post-text">
+                                <h4>Camping do Pico do Rinoceronte</h4>
+                                <div class="author-comments">
+                                    <span><i class="fa fa-user"></i>Robson Fernando Duda</span>
+                                </div>
+                                <!-- Horizontal Tela Detalhes Trilha -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-1229685353625953"
+                                     data-ad-slot="7739149091"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                     (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                                <br />
+                                <p>  
+                                    <p class="text-info"><i class="fa fa-info" aria-hidden="true"></i> Última atualização em 27 de agosto de 2022</p>
+                                    <p class="text-danger"><i class="fa fa-trash" aria-hidden="true"></i> Não jogue lixo</p>
+                                    <p class="text-success"><i class="fa fa-leaf" aria-hidden="true"></i> Proteja a natureza e os animais selvagens</p>
+                                </p>
                                 <p>
                                     Bom Jardim da Serra possui alguns dos principais atrativos da Serra Catarinense, atraindo turistas que procuram as belezas da região serrana. Entre mirantes e cânions, o visitante pode apreciar diferentes paisagens com diferentes níveis 
                                     de esforço. Já escrevemos sobre uma bela trilha da região que é a <strong><a href="https://trilhasemsc.com.br/bom-jardim-da-serra/trilhas/moderada/trilha-do-pico-do-rinoceronte">Trilha do Pico do Rinoceronte</a></strong>, que além de trilha,
@@ -130,92 +125,49 @@
                                 <p>
                                     Esperamos que as dicas dadas aqui ajudem você a conhecer Bom Jardim da Serra e ter uma experiência de camping, principalmente se escolher o Pico do Rinoceronte!
                                 </p>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-4 col-sm-12" style="text-align: center">
-                            <h5 class="mb-2 mt-2">Gostou do conteúdo?</h5>                          
-                            <p class="mb-2">Que tal nos pagar um café?</p>
-                            <a href="https://pag.ae/7-dSqqaS8" class="btn btn-sm bg-gradient-info btn-round mb-0 me-1"><i class="fa fa-coffee" style="font-size: 12px;" aria-hidden="true"></i> Pagar um café!</a>                            
-                        </div>
-                        <div class="col-lg-4 col-md-4 col-sm-12" style="text-align: center">
-                            <h5 class="mb-2 mt-2">Também pode ser um PIX</h5>   
-                            <p class="mb-2">Chave: trilhasemsc@gmail.com</p>     
-                            <img class="mx-auto" src="{{ asset('img/qrcode.png') }}" style="width: 80%;" alt="PIX TrilhasemSC">                                              
-                        </div>
-                        <!--
-                            <div class="col-lg-12 col-md-12 mt-5">
-                                <p><i class="fa fa-road text-success"></i> <strong>Distância</strong>: </p>
-                                <p><i class="fa fa-clock-o text-info"></i> <strong>Duração</strong>: </p>
-                                <p><i class="fa fa-sun text-warning"></i> <strong>Exposição</strong>: </p>
-                                <p><i class="fa fa-heartbeat text-danger"></i> <strong>Esforço</strong>: </p>
-                                <p><i class="fa fa-compass text-info"></i> <strong>Orientação</strong>: </p>
-                                <p><a href="{{ url('guia-de-dificuldade-em-trilhas/abnt') }}">Clique e entenda as medidas</a></p>
+                                
+                                <h5><strong>Texto</strong>: Robson Fernando Duda</h5>
                             </div>
-                        -->
-                    </div>
-                </div>
-               
-                @include('layouts/partes/publicidade-google')
-                <div class="row mb-3">
-                    <div class="col-lg-12 col-sm-12 mb-sm-0 mb-4">
-                       <div class="info-horizontal bg-gradient-primary border-radius-lg p-3">
-                          <i class="ni ni-calendar-grid-58 h4 text-white icon"></i>
-                          <div class="description ps-3">
-                             <h5 class="text-white">Atualização das Informações</h5>
-                             <p class="text-white">Fique sempre atento à data de atualização dos textos. Os textos são feitos com base em nossas trilhas e podem estar desatualizados em razão do tempo da última visita.</p>
-                          </div>
-                       </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12 col-sm-12 mb-sm-0 mb-4">
-                        <div class="info-horizontal bg-gradient-info border-radius-lg p-3">
-                           <i class="ni ni-compass-04 h4 text-white icon"></i>
-                           <div class="description ps-3">
-                              <h5 class="text-white">Contrate um Condutor</h5>
-                              <p class="text-white">Sempre que visitar um local desconhecido, procure por condutores locais autorizados. Faça turismo seguro.</p>
-                              <a href="{{ url('guias-e-condutores') }}">Lista de Condutores</a>
-                           </div>
+                             <h4 class="center"><a class="link" href="{{ url('/') }}">Voltar para o início</a></h4>
+                            <!-- Horizontal Tela Detalhes Trilha -->
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-client="ca-pub-1229685353625953"
+                                 data-ad-slot="7739149091"
+                                 data-ad-format="auto"
+                                 data-full-width-responsive="true"></ins>
+                            <script>
+                                 (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>                           
                         </div>
-                     </div>
-                 </div>    
-            </div>
-            <div class="col-md-3">            
-                <div class="col-lg-12 mt-4">
-                    <h4 class="mt-0">Busca por Cidade</h4>                    
-                        <label class="" style="font-size: 100%; margin: 0px;">Selecione a cidade</label>
-                        <p class="mt-0 text-danger" style="font-size: 85%;">São mostradas somente cidades que possuem alguma trilha cadastrada.</p>
-                        <select class="form-control mb-0" name="cidade" id="list-cidade">
-                           <option value="">Selecione a cidade</option>
-                           @if(isset($busca_cidade))
-                              @forelse($busca_cidade as $busca)
-                                 <option value="{{ url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista') }}">{{ $busca->cidade->nm_cidade_cde }} ({{ $busca->total }})</option>
-                              @empty
-                                 <option selected value="">Nenhuma cidade disponível</option>
-                              @endforelse
-                           @endif
-                        </select>
-                        @include('layouts/partes/publicidade-google')
+                    </div>
+                    <div class="col-md-3">
+                        <div class="sidebar-widget">
+                            <div class="single-sidebar-widget">
+                                <h4>PESQUISAR <span>Trilha</span></h4>
+                                <form id="text-search" action="{{url('trilhas/#lista')}}" >
+                                    <input type="text" name="termo" placeholder="Digite aqui">
+                                    <button class="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="single-sidebar-widget country-select">
+                                <h4>BUSCA POR <span>Cidade</span></h4>
+                                <ul class="widget-categories">
+                                    @foreach($busca_cidade as $busca)
+                                        <li><a href="{{url(stringToStringSeo($busca->cidade->nm_cidade_cde).'/trilhas/#lista')}}">{{ $busca->cidade->nm_cidade_cde }}<span>({{ $busca->total }})</span></a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+    
+                            <div class="col-lg-12 col-md-12 col-sm-12" style="min-height: 570px; background: #f1f1f1;display: none">
+                                <div class="box-publicidade-detalhes">
+                                    <span>PUBLICIDADE</span>
+                                </div>
+                            </div>                                
+                        </div>
+                    </div>
                 </div>
-            </div>         
-       </div>
-       <div class="row">
-            <div class="col-md-9 mt-2">
-            <!-- @env('production')
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-1229685353625953"
-                    data-ad-slot="7739149091"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>                
-            @endenv -->
             </div>
-       </div>
-    </div>
- </section>
+        </div>
 @endsection
