144376dea4968f26a80a56f7aba663af0063349f
Robson Fernando Duda <robsonferduda@gmail.com>
2026-02-22 14:44:08 -0300

    usuário - Trilha

--- resources/views/trilhas/detalhes-novo.blade.php
+++ resources/views/trilhas/detalhes-novo.blade.php
@@ -44,15 +44,15 @@
                     <h4 class="mt-2">{{ $trilha->nm_trilha_tri }}</h4>
                         <div class="d-flex align-items-center mb-2">
                             <img src="{{ asset('img/trilheiros/' . ($trilha->user->dc_foto_perfil ?? 'perfil.png')) }}" alt="Foto do usuário {{ $trilha->user->name ?? '' }}" class="rounded-circle me-2" style="width: 40px; height: 40px; object-fit: cover;">
-                            <span class="text-secondary">Registrada por {{ $trilha->user->name ?? 'Usuário desconhecido' }}</span>
+                            <span class="text-secondary">Trilha registrada por {{ $trilha->user->name ?? 'Usuário desconhecido' }}</span>
                         </div>
                     <h6>
                         <i class="ni ni-pin-3 text-danger"></i> {{ $trilha->cidade->nm_cidade_cde }} 
                         <span class=""><a href="https://www.instagram.com/trilhasemsc/?hl=pt-br" target="_BLANK" style="color: #e73177;"><i class="fa fa-instagram" aria-hidden="true"></i> @trilhasemsc</a></span>
                     </h6>
                     <p class="mb-1">
-                        <time datetime="{{ \Carbon\Carbon::parse($trilha->updated_at)->toDateString() }}" itemprop="dateModified" class="text-info">
-                            Data da trilha: {{ \Carbon\Carbon::parse($trilha->updated_at)->format('d/m/Y') }}
+                        <time datetime="{{ \Carbon\Carbon::parse($trilha->updated_at)->toDateString() }}" itemprop="dateModified" class="text-black">
+                            Data da publicação: {{ \Carbon\Carbon::parse($trilha->updated_at)->format('d/m/Y') }}
                         </time>
                     </p>
                     <p class="mb-1">
