2099b0917cd56ce520af53aa9dc93d4d45e33a4b
Robson Fernando Duda <robsonferduda@gmail.com>
2026-07-28 12:06:39 -0300

    Tracklog

--- resources/views/aventuras/peru.blade.php
+++ resources/views/aventuras/peru.blade.php
@@ -169,10 +169,10 @@
             <div class="timeline">
                 @php
                 $salkantay = [
-                    ['18/08', 'TER', 'Salkantay - Dia 1 - Soraypampa à Laguna Humantay', '', 'Tracklog do trajeto', 'https://loc.wiki/t/182583542?wa=sc'],
-                    ['19/08', 'QUA', 'Salkantay - Dia 2 - De Soraypampa até Chaullay (Colcapampa)', '', 'Tracklog do trajeto', 'https://loc.wiki/t/182699595?wa=sc'],
-                    ['20/08', 'QUI', 'Salkantay - Dia 3 - De Chaullay (Colcapampa) à Lucmabamba', '', 'Tracklog do trajeto', 'https://loc.wiki/t/182813083?wa=sc'],
-                    ['21/08', 'SEX', 'Salkantay - Dia 4 - De Lucmabamba à Macchu Picchu Pueblo (Aguas Calientes)', '', 'Tracklog do trajeto', 'https://loc.wiki/t/182950022?wa=sc'],
+                    ['18/08', 'TER', 'Salkantay - Dia 1 - Soraypampa à Laguna Humantay', '4km, sendo que fazemos ida e volta pelo mesmo trajeto. Partimos de 3800 e chegamos aos 4200 metros de altitude. Hospedagem em Soraypampa Hostel.', 'Tracklog do trajeto', 'https://loc.wiki/t/182583542?wa=sc'],
+                    ['19/08', 'QUA', 'Salkantay - Dia 2 - De Soraypampa até Chaullay (Colcapampa)', '20km, sendo o dia mais difícil, onde chegamos aos 4.600 metros de altitude. São 6km de subida e 14km de descida, rumo a Chaullay. Hospedagem em Hostel Samana Wasi Chaullay.', 'Tracklog do trajeto', 'https://loc.wiki/t/182699595?wa=sc'],
+                    ['20/08', 'QUI', 'Salkantay - Dia 3 - De Chaullay (Colcapampa) à Lucmabamba', '20km, sendo praticamente descidas, com perda de 800 metros de altitude. Hospedagem em Lucmabamba Lodge.', 'Tracklog do trajeto', 'https://loc.wiki/t/182813083?wa=sc'],
+                    ['21/08', 'SEX', 'Salkantay - Dia 4 - De Lucmabamba à Macchu Picchu Pueblo (Aguas Calientes)', '20km de descida, pasando por pontos como a Hidroelétrica e chegando em Aguas Calientes. Hospedagem em Samananchis Machupicchu.', 'Tracklog do trajeto', 'https://loc.wiki/t/182950022?wa=sc'],
                     ['22/08', 'SÁB', 'Salkantay - Dia 5 - Machu Picchu', 'Retorno para Cusco. Visita ao Machu Picchu.', '', ''],
                     ['23/08', 'DOM', 'Cusco', '', '', ''],
                 ];
